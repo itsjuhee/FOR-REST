16bd93cad6701401c5d8305e5a97a4f3a9de643c
linnchaean <106298174+linnchaean@users.noreply.github.com>
2022-07-22 09:53:39 +0900

    chaean_upload

---
+++ my-page.js
@@ -0,0 +1,12 @@
+
+
+    $('.my-nav p').on('click',function(){
+        $('.my-nav p').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.button p').on('click',function(){
+        $('.button p').removeClass('active');
+        $(this).addClass('active');
+    });
+
