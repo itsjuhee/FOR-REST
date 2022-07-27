a0114510e2e2bf75a4db132a1f1112bdc18d2dd0
chase <0809bora@naver.com>
2022-07-27 17:54:24 +0900

    booooooora0727

--- script/my-page.js
+++ script/my-page.js
@@ -1,12 +1,13 @@
 
 
-    $('.my-nav p').on('click',function(){
-        $('.my-nav p').removeClass('active');
-        $(this).addClass('active');
-    });
+    // $('.my-nav p').on('click',function(){
+    //     $('.my-nav p').removeClass('active');
+    //     $(this).addClass('active');
+    // });
 
     $('.button p').on('click',function(){
         $('.button p').removeClass('active');
         $(this).addClass('active');
     });
+
 
